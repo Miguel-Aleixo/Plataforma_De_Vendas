0d5683924566f82181722199684398e0b460b082
Miguel Oliveira Aleixo dos Santos <migueloliveiraaleixosantos1@gmail.com>
2025-12-07 16:22:13 -0300

    Ajustes para produção

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,30 +1,9 @@
-import fs from "fs";
-import path from "path";
-
-const dbPath = path.join(process.cwd(), "lib", "orders.json");
-
-// Garante que a pasta existe
-if (!fs.existsSync(path.dirname(dbPath))) {
-  fs.mkdirSync(path.dirname(dbPath), { recursive: true });
-}
-
-// Garante que o arquivo existe
-if (!fs.existsSync(dbPath)) {
-  fs.writeFileSync(dbPath, "{}");
-}
-
-export function loadDb() {
-  const raw = fs.readFileSync(dbPath, "utf-8");
-  return JSON.parse(raw);
-}
+const orders: Record<string, { email: string }> = {};
 
 export function saveOrder(orderId: string, email: string) {
-  const db = loadDb();
-  db[orderId] = { email };
-  fs.writeFileSync(dbPath, JSON.stringify(db, null, 2));
+  orders[orderId] = { email };
 }
 
 export function getEmailFromOrderId(orderId: string) {
-  const db = loadDb();
-  return db[orderId]?.email ?? null;
+  return orders[orderId]?.email ?? null;
 }
